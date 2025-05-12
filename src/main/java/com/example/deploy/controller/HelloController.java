c308ac6b3f27ff5837371e172126885a0dd42a80
LEONARDO VIEIRA GUIMARAES <leonardovieiraxy@hotmail.com>
2025-05-12 00:57:02 -0300

    Changes to be committed: 	modified:   docker-compose.yml 	modified:   pom.xml 	modified:   src/main/java/com/example/deploy/controller/HelloController.java 	new file:   src/main/java/com/example/deploy/controller/HomeController.java

--- src/main/java/com/example/deploy/controller/HelloController.java
+++ src/main/java/com/example/deploy/controller/HelloController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-     @GetMapping("/")
+
+    @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
+        return String.format("Hello %s!", name);
     }
-    
 }
