8f5404dce7b4a745b4ac8615f2cdb4e154e1c8d7
LEONARDO VIEIRA GUIMARAES <leonardovieiraxy@hotmail.com>
2025-05-20 01:57:06 -0300

    Descrição das alterações 5

--- src/test/java/com/example/deploy/DeployApplicationTests.java
+++ src/test/java/com/example/deploy/DeployApplicationTests.java
@@ -4,11 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.example.deploy.model.Produto;
 import com.example.deploy.service.ProdutoService;
 
 @SpringBootTest
+@ActiveProfiles("test") // Use o perfil de teste
 class DeployApplicationTests {
 
     @Autowired
