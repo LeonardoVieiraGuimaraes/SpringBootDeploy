5d7edeefd1bec3f7f16d8bc82b980bf4074839ef
LEONARDO VIEIRA GUIMARAES <leonardovieiraxy@hotmail.com>
2025-05-20 00:46:45 -0300

    deploy

--- src/test/java/com/example/deploy/DeployApplicationTests.java
+++ src/test/java/com/example/deploy/DeployApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.deploy;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.deploy.service.ProdutoService;
 
 @SpringBootTest
 class DeployApplicationTests {
 
+    @Autowired
+    private ProdutoService produtoService;
+
     @Test
     void contextLoads() {
+        assertThat(produtoService).isNotNull();
     }
-
 }
