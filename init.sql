384b2309b6559f3538870415846ab2927c3f3267
LeonardoVieiraGuimaraes <leonardovieiraxy@hotmail.com>
2025-05-21 15:00:56 -0300

    Changes to be committed: 	modified:   .github/workflows/deploy.yml 	modified:   docker-compose.yml 	deleted:    init.sql 	modified:   src/main/resources/application.yaml

--- init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS usuario (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE
-);
-
-INSERT INTO usuario (nome, email) VALUES ('Admin', 'admin@exemplo.com');
